63973db35175d162c78f676b70ccefb6d486fef6
Alexandr <dolbilov53@gmail.com>
2019-03-30 17:31:39 +0700

    Add default execute handler with [--help] [--output=<console|text|html>]

--- main.cpp
+++ main.cpp
@@ -1,37 +1,9 @@
 #include "output.hpp"
 #include <iostream>
-#include <string>
-
-using std::cout;
-using std::endl;
-
-// const list of params
-// "--help" for description of program
-const std::string sParam[]{"--help", "--output=console", "--output=text"};
+#include <string.h>
 
 int main(int argc, char const* argv[]) {
-    // == EXECUTE HANDLER
-
-    // list of enabled params,
-    bool* bParam = new bool[sizeof(sParam)]{false};
-
-    // if program exeuted with params
-    if (argc > 1) {
-        for (int i = 1; i < argc; i++) {
-            for (unsigned int j = 0; j < sizeof(sParam); j++) {
-                if (argv[i] == sParam[j])
-                    bParam[j] = true;
-            }
-        }
-    }
-
-    // if user need help
-    if (bParam[0]) {
-        cout << "CHESSVIZ_9000 RC" << endl;
-        cout << "program for check permissibility of chess match" << endl;
-        cout << "chessviz [--help] [--output=<console|text>]" << endl;
-    }
-
+    // TODO: normal board initialize
     board b = {
             'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r', 'p', 'p', 'p', 'p', 'p',
             'p', 'p', 'p', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
@@ -40,12 +12,21 @@ int main(int argc, char const* argv[]) {
             'P', 'P', 'P', 'P', 'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R',
     };
 
-    // output = text
-    if (bParam[1])
-        OutputConsole(b);
+    // basic execute handler
 
-    if (bParam[2])
-        OutputText(b);
+    if (argc > 1)
+        for (int i = 1; i < argc; i++) {
+            // check for "--help"
+
+            if (!strcmp(argv[i], "--help"))
+                OutputHelp();
+            if (!strcmp(argv[i], "--output=console"))
+                OutputConsole(b);
+            if (!strcmp(argv[i], "--output=text"))
+                OutputText(b);
+            if (!strcmp(argv[i], "--output=html"))
+                OutputHTML(b);
+        }
 
     return 0;
 }
