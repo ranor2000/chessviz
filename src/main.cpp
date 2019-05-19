d63f528a86eadc4ac1d5baf0feebd1d0fa5ffe5c
Alexandr <dolbilov53@gmail.com>
2019-05-20 05:14:44 +0700

    Fix main program for new format

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-
+#include "board.hpp"
 #include "board_print_console.hpp"
 #include "board_print_html.hpp"
 #include "board_print_text.hpp"
@@ -7,14 +7,8 @@
 
 int main(int argc, char const* argv[]) {
     // TODO: normal board initialize
-    board b = {
-            'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r', 'p', 'p', 'p', 'p', 'p',
-            'p', 'p', 'p', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
-            ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',
-            ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'P', 'P', 'P', 'P',
-            'P', 'P', 'P', 'P', 'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R',
-    };
-
+    Board board;
+    ResetBoard(board);
     // basic execute handler
 
     if (argc > 1)
@@ -24,11 +18,11 @@ int main(int argc, char const* argv[]) {
             if (!strcmp(argv[i], "--help"))
                 OutputConsoleHelp();
             else if (!strcmp(argv[i], "--output=console"))
-                OutputConsoleBoard(b);
+                OutputConsoleBoard(board);
             else if (!strcmp(argv[i], "--output=text"))
-                OutputFileText(b);
+                OutputFileText(board);
             else if (!strcmp(argv[i], "--output=html"))
-                OutputFileHTML(b);
+                OutputFileHTML(board);
             else
                 cout << "Wrong arguments!" << endl;
         }
